0621fcf39c1386188c45647c31522916b4ec42e5
Anastasia Kholod <nastunia.h@gmail.com>
2021-11-20 13:50:43 +0200

    Initial Commit

---
+++ DoorsTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DoorsTest
+//
+//  Created by Macbook Pro 13 on 20.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
